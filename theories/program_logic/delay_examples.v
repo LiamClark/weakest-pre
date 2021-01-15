548a02a5b55ebb7c1aaf6328dc82f7d3cf83ec3e
LiamClark <L.A.Clark@student.tudelft.nl>
2021-01-15 17:59:09 +0100

    Verify the fib example
    The trick was to use strong induction to rewrite the post conditions.

--- theories/program_logic/delay_examples.v
+++ theories/program_logic/delay_examples.v
@@ -19,14 +19,27 @@ Proof.
   reflexivity.
 Qed.
 
+(* 
+    What is the intuition behind this?
+*)
 Fixpoint coq_fib (n1 n2: nat) (n: nat): nat :=
     match n with
     |O => n1
     |S n' => match n' with
              | O => n2 
-             | S n'' => coq_fib n1 n2 n' + coq_fib n1 n2 n''
+             | S n'' => (coq_fib n1 n2 n') + (coq_fib n1 n2 n'')
              end
     end.
+
+Lemma test_coq_fib:
+  (coq_fib 0 1) <$>[0; 1; 2; 3; 4; 5; 6; 7] = [0; 1; 1; 2; 3; 5; 8; 13].
+  reflexivity.
+Qed.
+
+Lemma test_coq_fib_move: coq_fib 13 21 4 = coq_fib 5 8 (S (S 4)).
+Proof.
+    reflexivity.
+Qed.
 
 Section state_ad.
 Context `{! inG Σ (heapR natO)}.
@@ -35,45 +48,49 @@ Lemma post' (n1 n2 n: nat) (ret: nat): iProp Σ.
 refine( ⌜ret = coq_fib n1 n2 n⌝%I).
 Defined.
 
-Locate "+".
-Lemma post'' (n1 n2 n: nat) (res: (nat * nat * nat) + nat): iProp Σ.
-refine (match n with
-        |O    => ⌜res = inr n1⌝
-        |S n' => ⌜res = inl (n', n2, Nat.add n1 n2)⌝ 
-end)%I.
-Defined.
-
-(* one execution of fib either returns the addition and takes one from n
-   or n is zero and returns the answer 
-*)
-Lemma verify_fib' n1 n2 n: 
-    True -∗ wp_delay (fib' (n, n1, n2)) (post'' n1 n2 n).
+Lemma coq_fib_unfold n1 n2 n: 
+    coq_fib n1 n2 (S (S n)) = coq_fib n1 n2 (S n) + coq_fib n1 n2 n.
 Proof.
-    iIntros "_".
-    unfold fib'. unfold post''.
-    destruct n.
-    - by iApply wp_delay_return.
-    - by iApply wp_delay_return.
-Qed.
-
-(* If i pick a particular n I can just unroll the loop n times*)
-Lemma verify_delay_fib_brute:
-    True -∗ @wp_delay Σ _ (fib 4) (λ ret, ⌜ret = 3⌝).
-Proof.
-    iIntros "_".
-    unfold fib.
-    iApply wp_delay_iter. 
-    iApply wp_delay_return.
-    iApply wp_delay_iter.
-    iApply wp_delay_return.
-    iApply wp_delay_iter.
-    iApply wp_delay_return.
-    iApply wp_delay_iter.
-    iApply wp_delay_return.
-    iApply wp_delay_iter.
-    iApply wp_delay_return.
     done.
 Qed.
+
+Lemma pair_induction (P : nat -> Prop): 
+    P 0 ->
+    P 1 ->
+    (forall n, P n -> P (S n) -> P (S (S n))) ->
+    forall n, P n.
+Proof.
+    intros H0 H1 Hstep n.
+    assert (P n /\ P (S n)).
+    induction n.
+    - done.
+    - destruct IHn. split.
+        + done. 
+        + by apply Hstep.
+    - by destruct H.
+Qed.
+
+SearchAbout plus.
+Lemma coq_fib_move n1 n2 n:
+ coq_fib n2 (n1 + n2) (S n) = coq_fib n1 n2 (S (S n)).
+Proof.
+    induction n using pair_induction.
+    - by rewrite Nat.add_comm.
+    - by rewrite Nat.add_comm.
+    - rewrite -> (coq_fib_unfold  n1 n2 (S (S (n)))).
+      rewrite <- IHn0.
+      rewrite <- IHn.
+      rewrite -> coq_fib_unfold.
+      reflexivity.
+Qed.
+
+Lemma coq_fib_move' n1 n2 n:
+ coq_fib n2 (n1 + n2) n = coq_fib n1 n2 (S n).
+ Proof.
+     destruct n.
+     + done.
+     + apply coq_fib_move.
+ Qed.
 
 (* To get lob induction to work I need the numbers that are passed
     between loop states to vary. but then my post condition does
@@ -84,18 +101,16 @@ Lemma verify_delay_fib' n1 n2 n:
 Proof.
     (*How do i vary the IH here properly *)
     iLöb as "IH" forall (n n1 n2).
-    unfold fib.
     iApply wp_delay_iter. 
-    destruct n eqn: E.
+    destruct n as [| n'] eqn: E .
     - iApply wp_delay_return. done.
     - iApply wp_delay_return. simpl. 
       iNext.
       iApply (wp_strong_mono_delay with "IH").
       unfold post'.
       iIntros (v Hv) "!%".
-      simpl.
-
-      iDestruct ("IH" $! n0 n2) as "IH'".
+      subst v.
+      apply coq_fib_move'.
 Qed.
 
 Lemma verify_delay_fib n:
