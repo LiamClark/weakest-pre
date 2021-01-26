a5f36948cb205ca9b70c668283fc0e46f3630aea
LiamClark <L.A.Clark@student.tudelft.nl>
2021-01-26 15:39:12 +0100

    Finish example multithreaded evaluator

--- theories/program_logic/delayfree.v
+++ theories/program_logic/delayfree.v
@@ -5,7 +5,10 @@ Require Import Unicode.Utf8.
 
 (*
 This is the type algebra normalized form of:
-∀ S, iTree (State S).
+∀ S, iTree (stateE S).
+
+Do I need partiality on the get and put signatures? or can that be handled
+completely at the semantic level?
 *)
 CoInductive delayST (ST A: Type): Type :=
 | Answer: A -> delayST ST A
@@ -81,17 +84,33 @@ Definition step_delayST_threads {ST A}
                         end
         end.
 
-Fixpoint eval_delayST {ST A} 
+Definition check_delayST {ST A} (e: delayST ST A): A + delayST ST A :=
+    match e with
+    | Answer x  => inl x
+    | Get k     => inr $ e
+    | Put s' k  => inr $ e
+    | Think e'  => inr $ e
+    end.
+
+(* Todo check the order of effects with list and state here
+   and remove all these nested pattern matches
+*)
+Fixpoint eval_threaded_delayST {ST A} 
     (n: nat) 
     (threads: list (delayST ST A))
-    (s: ST) {struct n}: option A.
-refine (
+    (s: ST) {struct n}: option A :=
+
     match n with
     | O => None
-    | S n' => let (_, threads') := (step_delayST_threads threads s)
-              in _
-    end
-).
+    | S n' => let (s', threads') := (step_delayST_threads threads s)
+              in match threads' with
+                 | nil => None
+                 | cons e' _ => match check_delayST e' with
+                                | inl x => Some x
+                                | inr e'' => eval_threaded_delayST n' threads' s'
+                                end
+                 end
+    end.
 
 
 
