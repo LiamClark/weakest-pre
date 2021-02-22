08d843d9f03404401367857f8e0e5d6fc086394c
LiamClark <L.A.Clark@student.tudelft.nl>
2021-02-22 11:59:40 +0100

    Proofs for first wp rules
    There is a lot of duplication in the bind proof. Every command has an
    identical proof. Perhaps that's extractable.

--- theories/program_logic/delayfreewp.v
+++ theories/program_logic/delayfreewp.v
@@ -6,12 +6,12 @@ From shiris.program_logic Require Import delayfree.
 Set Default Proof Using "Type".
 
 (* Curry the value R so it can be changed by the dependent pattern match on c *)
-Definition foo {V R} (c: envE V R) (σ σ': gmap loc V): R -> Prop.
+Definition command_predicate {V R} (c: envE V R) (σ σ': gmap loc V): R -> Prop.
 refine (match c with
         |GetE l  => λ v, σ !! l = Some v /\ σ' = σ 
         |PutE l v' => λ _, is_Some (σ !! l) /\ σ' = <[l := v']> σ
-        |AllocE v' => λ _, True
-        |FreeE l => λ _, True 
+        |AllocE v' => λ l, σ !! l = None /\ σ' = <[l := v']> σ
+        |FreeE l => λ _, is_Some (σ !! l) /\ σ' = delete l σ
         end
 ).
 Defined.
@@ -24,7 +24,7 @@ refine(λ R e Φ,
         |Answer x => Φ x 
         |Think e' => ▷ go R e' Φ
         |Fork e' k => ▷ (go R k Φ ∗ go unit e' (λ _, True))
-        |Vis c k => ∀σ, SI σ ==∗ ∃σ' v, ⌜foo c σ σ' v⌝ ∗ SI σ' ∗ ▷ (go R (k v)) Φ
+        |Vis c k => ∀σ, SI σ ==∗ ∃σ' v, ⌜command_predicate c σ σ' v⌝ ∗ SI σ' ∗ ▷ (go R (k v)) Φ
         end
 )%I.
 Defined.
@@ -41,3 +41,84 @@ Definition wp' {Σ} {V} (SI: gmap nat V -> iProp Σ)
 
 Definition wp {Σ} {V R} (SI: gmap nat V -> iProp Σ) (e: expr V R) (Φ: R -> iProp Σ): iProp Σ := 
     wp' SI R e Φ.
+
+Lemma wp_unfold {Σ} {V R} (e: expr V R) (SI: gmap nat V -> iProp Σ) (Φ: R -> iProp Σ)
+  : wp SI e Φ ≡  wp_pre SI (wp' SI) R e Φ.
+Proof.
+  apply (fixpoint_unfold (wp_pre SI)).
+Qed.
+
+Section wp_rules.
+  Context {Σ} {V R: Type} (SI: gmap nat V -> iProp Σ).
+
+  Lemma wp_return (x: R) (Φ: R -> iProp Σ): Φ x -∗ wp SI (mret x) Φ.
+  Proof.
+    iIntros"H".
+    by rewrite wp_unfold.
+  Qed.
+
+  Lemma wp_think (e: expr V R) (Φ: R -> iProp Σ): ▷ wp SI e Φ -∗ wp SI (Think e) Φ.
+  Proof.
+    iIntros "Hwp".
+    iEval (rewrite wp_unfold). 
+    done.
+  Qed.
+
+ (*
+   This proof is huge becasue of the case distinction on commands
+   is that avoidable?
+ *)
+  Lemma wp_bind {B} (f: R -> expr V B) (Φ: B -> iProp Σ) (e: expr V R): 
+    wp SI e (λ x, wp SI (f x) Φ) -∗ wp SI (e ≫= f) Φ.
+  Proof.
+    unfold mbind, itree_bind.
+    iIntros "H". iLöb as "IH" forall (e).
+    iEval (rewrite wp_unfold).
+    unfold wp_pre.
+    destruct e; simpl.
+    - do 2 (rewrite wp_unfold /=). unfold wp_pre.
+      done.
+    - iEval (rewrite wp_unfold /=) in "H".
+      iNext. iApply "IH". done.
+    - iEval (rewrite wp_unfold /=) in "H".
+      iNext. iDestruct "H" as "(H & $)". 
+      iApply "IH". done.
+    - iIntros (σ)  "HSi".
+      destruct e.
+      + iEval (rewrite wp_unfold /=) in "H".
+        iMod ("H" $! σ with "HSi") as "H". iModIntro.
+        iDestruct ("H") as (σ' v) "H".
+        iExists σ', v.
+        iDestruct "H" as (Hlookup) "(HSi' & Hwp')". 
+        iSplit.
+        * iPureIntro. done.
+        * iFrame. iNext. iApply "IH". done.
+      + 
+        iEval (rewrite wp_unfold /=) in "H".
+        iMod ("H" $! σ with "HSi") as "H". iModIntro.
+        iDestruct ("H") as (σ' vtt) "H".
+        iExists σ', vtt.
+        iDestruct "H" as (Hput) "(HSi' & Hwp')". 
+        iSplit.
+        * iPureIntro. done.
+        * iFrame. iNext.
+          iApply "IH". done.
+      + iEval (rewrite wp_unfold /=) in "H".
+        iMod ("H" $! σ with "HSi") as "H". iModIntro.
+        iDestruct ("H") as (σ' vtt) "H".
+        iExists σ', vtt.
+        iDestruct "H" as (Hput) "(HSi' & Hwp')". 
+        iSplit.
+        * iPureIntro. done.
+        * iFrame. iNext.
+          iApply "IH". done.
+      + iEval (rewrite wp_unfold /=) in "H".
+        iMod ("H" $! σ with "HSi") as "H". iModIntro.
+        iDestruct ("H") as (σ' vtt) "H".
+        iExists σ', vtt.
+        iDestruct "H" as (Hput) "(HSi' & Hwp')". 
+        iSplit.
+        * iPureIntro. done.
+        * iFrame. iNext.
+          iApply "IH". done.
+  Qed.
